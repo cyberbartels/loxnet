6f2a37bae58b9fad566fd3153337132fcdd7be50
cyberbartels <71546296+cyber-bartels@users.noreply.github.com>
2022-10-23 19:41:11 +0200

    Add scan capabilities

--- loxnet/Scanner.cs
+++ loxnet/Scanner.cs
@@ -27,11 +27,70 @@ namespace de.softwaremess.loxnet
             {
                 // We are at the beginning of the next lexeme.
                 start = current;
-                //scanToken();
+                scanToken();
             }
 
             tokens.Add(new Token(TokenType.EOF, "", null, line));
             return tokens;
+        }
+
+        private void scanToken()
+        {
+            char c = Advance();
+            switch (c)
+            {
+                case '(': AddToken(TokenType.LEFT_PAREN); break;
+                case ')': AddToken(TokenType.RIGHT_PAREN); break;
+                case '{': AddToken(TokenType.LEFT_BRACE); break;
+                case '}': AddToken(TokenType.RIGHT_BRACE); break;
+                case ',': AddToken(TokenType.COMMA); break;
+                case '.': AddToken(TokenType.DOT); break;
+                case '-': AddToken(TokenType.MINUS); break;
+                case '+': AddToken(TokenType.PLUS); break;
+                case ';': AddToken(TokenType.SEMICOLON); break;
+                case '*': AddToken(TokenType.STAR); break;
+                case '!':
+                    AddToken(Match('=') ? TokenType.BANG_EQUAL : TokenType.BANG);
+                    break;
+                case '=':
+                    AddToken(Match('=') ? TokenType.EQUAL_EQUAL : TokenType.EQUAL);
+                    break;
+                case '<':
+                    AddToken(Match('=') ? TokenType.LESS_EQUAL : TokenType.LESS);
+                    break;
+                case '>':
+                    AddToken(Match('=') ? TokenType.GREATER_EQUAL : TokenType.GREATER);
+                    break;
+
+                default:
+                    Lox.Error(line, "Unexpected character.");
+                    break;
+            }
+        }
+
+        private char Advance()
+        {
+            return source[current++];
+        }
+
+        private bool Match(char expected)
+        {
+            if (isAtEnd()) return false;
+            if (source[current] != expected) return false;
+
+            current++;
+            return true;
+        }
+
+        private void AddToken(TokenType type)
+        {
+            AddToken(type, null);
+        }
+
+        private void AddToken(TokenType type, Object literal)
+        {
+            String text = source.Substring(start, current - start);
+            tokens.Add(new Token(type, text, literal, line));
         }
 
         private bool isAtEnd()
