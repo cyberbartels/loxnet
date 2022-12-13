db1de9783d748725e837146911d8393463c91e5a
cyberbartels <71546296+cyber-bartels@users.noreply.github.com>
2022-12-13 18:28:23 +0100

    Add some tests for statements (print, assign, while, for)

--- loxnetTests/InterpreterTests.cs
+++ loxnetTests/InterpreterTests.cs
@@ -41,5 +41,57 @@ namespace de.softwaremess.loxnetTests
             interpreter.Interpret(stmts);
         }
 
+        [Test]
+        public void CanInterpretPrint()
+        {
+            Scanner scanner = new Scanner("print \"test\"; ");
+            List<Token> tokens = scanner.ScanTokens();
+
+            Parser parser = new Parser(tokens);
+            List<Stmt> stmts = parser.Parse();
+
+            Interpreter interpreter = new Interpreter();
+            interpreter.Interpret(stmts);
+        }
+
+        [Test]
+        public void CanInterpretAssignment()
+        {
+            Scanner scanner = new Scanner("var a = 0; print a; ");
+            List<Token> tokens = scanner.ScanTokens();
+
+            Parser parser = new Parser(tokens);
+            List<Stmt> stmts = parser.Parse();
+
+            Interpreter interpreter = new Interpreter();
+            interpreter.Interpret(stmts);
+        }
+
+        [Test]
+        public void CanInterpretWhileLoop()
+        {
+            Scanner scanner = new Scanner("var a = 0;\r\nwhile(a < 100 ) {\r\n  print a;\r\n  a = a + 1;\r\n}");
+            List<Token> tokens = scanner.ScanTokens();
+
+            Parser parser = new Parser(tokens);
+            List<Stmt> stmts = parser.Parse();
+
+            Interpreter interpreter = new Interpreter();
+            interpreter.Interpret(stmts);
+        }
+
+        [Test]
+        public void CanInterpretFibonacciProgram()
+        {
+            Scanner scanner = new Scanner("var a = 0;\r\nvar temp;\r\n\r\nfor (var b = 1; a < 10000; b = temp + b) {\r\n  print a;\r\n  temp = a;\r\n  a = b;\r\n}");
+            List<Token> tokens = scanner.ScanTokens();
+
+            Parser parser = new Parser(tokens);
+            List<Stmt> stmts = parser.Parse();
+
+            Interpreter interpreter = new Interpreter();
+            interpreter.Interpret(stmts);
+        }
+
     }
 }
